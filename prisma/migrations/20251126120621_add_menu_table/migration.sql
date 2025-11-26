a3d5fc84ab86b417ae28243da439064e140a85e0
davudumonnn <nawwafzayyan27@gmail.com>
2025-11-26 19:24:41 +0700

    initial commit

--- prisma/migrations/20251126120621_add_menu_table/migration.sql
+++ prisma/migrations/20251126120621_add_menu_table/migration.sql
@@ -2,12 +2,13 @@
 CREATE TABLE `Menu` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
+    `category` VARCHAR(191) NOT NULL,
     `price` INTEGER NOT NULL,
     `calories` INTEGER NOT NULL,
     `ingredients` JSON NOT NULL,
     `description` VARCHAR(191) NOT NULL,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
